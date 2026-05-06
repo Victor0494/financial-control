8c103e2e0f68ef85a16a5c30a7c6780741a7795e
victor martins <vmartins@fairfax.com.br>
2026-05-06 09:07:36 -0300

    Implementacao da formatacao de data

--- src/app/components/bill/bill/bill.component.ts
+++ src/app/components/bill/bill/bill.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { BillDTO } from './bilDTO';
 import { CurrencyPipe } from '@angular/common';
-import { AddMoneyService } from '../../../services/addMoney/add-money.service';
 import { BillService } from '../../../services/bill/bill.service';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-bill',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, DatePipe],
   templateUrl: './bill.component.html',
   styleUrl: './bill.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localePtBr from '@angular/common/locales/pt';
+
+registerLocaleData(localePtBr);
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
